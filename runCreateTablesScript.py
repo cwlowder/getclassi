ff45a41b54795d1aec96decd97e0d9a33354e36e
Julian Nieto <nieto225@gmail.com>
2018-10-23 19:43:45 -0500

    fixed typos

--- runCreateTablesScript.py
+++ runCreateTablesScript.py
@@ -4,7 +4,7 @@ import getpass
 
 if len(sys.argv) < 3:
 	print("Please give file name and database name")
-	print(python runCreateTableScript.py [File Name] [Database Name]
+	print("python runCreateTableScript.py [File Name] [Database Name]")
 	exit()
 f = open(sys.argv[1], "r")
 print(sys.argv[2])
