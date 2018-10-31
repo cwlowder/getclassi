893ed6c662dc5038af630c1ccc95a741ef273962
cwlowder <no>
2018-10-30 20:32:51 -0500

    Finished drop class API

--- backend/API/drop_class.py
+++ backend/API/drop_class.py
@@ -1,6 +1,8 @@
 from CONSTANTS import *
 import json
 from urllib.parse import parse_qs as pq
+import DB.database as db
+from traceback import print_exc
 
 def check_request(environ, start_response):
 	query = pq(environ[QUERY])
@@ -10,11 +12,11 @@ def check_request(environ, start_response):
 			STATUS: FAILED,
 			MESSAGE: "Bad request method: expecting POST"
 		})
-	elif "class" not in query or len(query["class"]) == 0:
+	elif "crn" not in query or len(query["crn"]) == 0:
 		start_response('400 Bad Request', [('Content-Type', 'json')])
 		return json.dumps({
 			STATUS: FAILED,
-			MESSAGE: "Missing query parameter ?class=%"
+			MESSAGE: "Missing query parameter ?crn=%"
 		})
 	else:
 		return ""
@@ -32,7 +34,44 @@ def dummy(environ, start_response):
 	return [message.encode()]
 
 def real(environ, start_response):
-	raise Exception('Todo implement real drop_class api')	
+	print("MAC ON THE ATTACK: quietiepie")
+	message = check_request(environ,start_response)
+	query = pq(environ[QUERY])
+	if message == "":
+		crn = query["crn"][0]
+		sql = "DELETE FROM Enrollments WHERE CRN = %s and NetId=%s"
+		val = (crn, "0") #TODO CHANGE TO LOGGED IN USER
+		mydb = None
+		try:
+			mydb, mycursor = db.connect()
+			#db.mydb.start_transaction(isolation_level=SERIALIZABLE, readonly=False)
+			mycursor.execute(sql, val)
+			if mycursor.rowcount > 0:
+				start_response('200 OK', [('Content-Type', 'json')])
+				mydb.commit()
+				message = json.dumps({
+					STATUS: SUCCESS
+				})
+			else:
+				start_response('400 Bad Request', [('Content-Type', 'json')])
+				mydb.commit()
+				message = json.dumps({
+					STATUS: FAILED,
+					MESSAGE: "User is not enrolled in class"
+				})
+		except Exception as e:
+			db.mydb.rollback()
+			print_exc()
+			start_response('500 INTERNAL SERVER ERROR', [('Content-Type', 'json')])
+			message = json.dumps({
+				STATUS: FAILED,
+				MESSAGE: str(e)
+			})
+		finally:
+			if mydb:
+				mydb.close()
+	print(message)
+	return [message.encode()]
 
 def drop_class(environ, start_response):
 	if DUMMY_MODE:
