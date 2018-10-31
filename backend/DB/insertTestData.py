9632110d79ca3a6c84ca236129948ad53ecfd517
cwlowder <no>
2018-10-31 00:02:11 -0500

    Changed insert

--- backend/DB/insertTestData.py
+++ backend/DB/insertTestData.py
@@ -30,13 +30,13 @@ def testData():
 	sql =  "INSERT INTO Events (EventId, CRN, Title, DueDate, Event_Des) VALUES (%s, %s, %s, %s, %s)"
 	current = time.time()
 	for x in range(0, 50):
-		val += [(x, str(x % 10), "MP" + str(x), time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(current + 100 * x)), "EventId is that " + str(x))]
+		val += [(x, str(x % 10), "MP" + str(x), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current + (100 * x + x)%8000)), "EventId is that " + str(x))]
 	db.mycursor.executemany(sql, val)
 
 	val = []
 	sql =  "INSERT INTO Enrollments (CRN, NetId) VALUES (%s, %s)"
 	for x in range(0, 30):
-		val += [(str(x % 10), str(int(x/3) % 10))]
+		val += [(str(x % 10), str(int(x) % 10))]
 	db.mycursor.executemany(sql, val)
 	db.mydb.commit()
 
