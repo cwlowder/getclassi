ff73fffe30b2b95c44860ef21ba29b99c7dee774
cwlowder <no>
2018-11-08 01:26:52 -0600

    Removing print

--- backend/API/set_note.py
+++ backend/API/set_note.py
@@ -45,7 +45,6 @@ def real(environ, start_response):
 	if message == "":
 		crn = query["crn"][0]
 		body = environ[BODY].read().decode("utf-8")
-		print("body", body)
 		body = json.loads(body)
 		sql = "UPDATE Classes SET Note = %s WHERE CRN = %s"
 		if 'note' not in body or str(type(body['note'])) != "<class 'str'>" or len(body['note']) > 280:
