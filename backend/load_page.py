21295604ab962f338391e39d63d89c5c5315bfbb
cwlowder <no>
2018-11-26 13:03:43 -0600

    Done?

--- backend/load_page.py
+++ backend/load_page.py
@@ -21,7 +21,7 @@ def load_page(environ, start_response):
 		# This is a development server
 		pass
 	# Assume this is a production server
-	if 'HTTPS' not in environ:
+	if 'HTTPS' not in environ or environ['HTTPS'].lower() != 'on':
 		# Redirect to https
 		dest = "https://" + environ['HTTP_HOST'] + environ[PATH]
 		print("dest:", dest)
